ea15d98b89040343b6d05c3df27e1fe3aa2fea70
IAmOscar-Liu <karta0989006@gmail.com>
2020-06-08 16:35:52 +0800

    change sth

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.send("Sample App");
-})
+//app.get("/", (req, res) => {
+//    res.send("Sample App");
+//})
 
 app.get("/getData", (req, res) => {
     res.json(
